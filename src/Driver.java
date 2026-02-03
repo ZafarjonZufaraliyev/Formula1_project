86cfd19cf5b506db4fa56ce8e071c445ac15ba4f
Zafarjon Zufaraliyev <zufaraliyevz@gmail.com>
2026-02-04 01:10:13 +0500

    Formula 1 loyihasi qilishni  boshladik...

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,7 @@
 public class Driver {
     private String name;
     private MyArray<Time> timeList=new MyArray<>();
+    private int points[]={25, 18, 15, 12, 10, 8, 6, 4 ,2 ,1};
     public String getName() {
         return name;
     }
@@ -18,5 +19,14 @@ public class Driver {
             gps.add(time.getGp());
         }
         return gps;
+    }
+    public int getPoints(){
+        int total=0;
+        for (Time time:timeList){
+            GP gp=time.getGp();
+            int position= gp.getPosition(this);
+            total=points[position-1]+total;
+        }
+        return total;
     }
 }
